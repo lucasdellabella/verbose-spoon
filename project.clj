f634a7de44f9c72edd974f01dd1028c7efe34b8c
Lucas Della Bella <dellabella.lucas@gmail.com>
2016-10-28 18:30:46 -0400

    Initial commit. Hot realoading working.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject phase3 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [ring "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [compojure "1.5.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler phase3.routes/app})
---
+++ src/phase3/routes/handler.clj
@@ -0,0 +1,17 @@
+(ns phase3.routes.handler
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+
+(defroutes routes
+  (GET "/login" [] "Login page replaces this string") ;;Put your login page here
+  (route/not-found "<h1>Page not found</h1>"))
+
+(def handler
+  (-> #'routes
+      wrap-reload))
+
+(defonce server (jetty/run-jetty handler {:port 8080 :join? false}))
