9f2fdb63ff85fbf06bc87dd8dc2f51be00f98a92
yavila <yjas3@gatech.edu>
2016-12-05 17:49:24 -0500

    Front-end for multiple courses on MAIN page added.

--- resources/public/js/add-category.js
+++ resources/public/js/add-category.js
@@ -1,18 +1,24 @@
+var catCount = 1;
 function addCategory (options) {
-  var newTableRow = document.createElement("td");
+  if (catCount < 9) {
+    var newTableRow = document.createElement("td");
+    // create select drop-down
+    select = document.createElement("select");
+    select.name = "category" + catCount;
+    catCount = catCount + 1;
 
-  // create select drop-down
-  select = document.createElement("select");
+    for (var i = 0; i < options.length-1; i++) { // last entry empty string
+      var opt = options[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.appendChild(el);
+    }
 
-  for (var i = 0; i < options.length-1; i++) { // last entry empty string
-    var opt = options[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
+    // add the created element and its content into the DOM
+    var currentEl = document.getElementById("category");
+    currentEl.insertBefore(select, currentEl.lastChild);
+  } else {
+    alert("Max Categories Reached.")
   }
-
-  // add the newly created element and its content into the DOM
-  var currentEl = document.getElementById("category");
-  currentEl.insertBefore(select, currentEl.lastChild);
 }
--- src/verbose_spoon/views/main.clj
+++ src/verbose_spoon/views/main.clj
@@ -1,14 +1,22 @@
 (ns verbose-spoon.views.main
-  (:require [hiccup.page :refer [html5]]
+  (:require [hiccup.page :refer [html5 include-js]]
             [hiccup.form :as f]
             [hiccup.element :as e]
             [verbose-spoon.model.core :refer [fetch-major-list fetch-category-list fetch-designation-list]]))
+
+; Move these functions outta here
+(defn format-categories []
+  (apply str (map (fn [l] (str l "','")) (fetch-category-list))))
+
+(defn create-categories-js-func []
+  (str "addCategory(['" (format-categories) "'])"))
 
 (defn page
   []
   (html5
     [:head
-      [:title "Main Page"]]
+      [:title "Main Page"]
+      (include-js "/js/add-category.js")]
     [:body
       [:h1 "Main Page"]
       [:div
@@ -16,12 +24,12 @@
           [:button "Me"]]]
       [:div
         (f/form-to [:post ""]
-          [:div
+          [:div {:id "category"}
               (f/label :title "Title:")
               (f/text-field :title)
               (f/label :category "Category:")
               (f/drop-down :category-drop (fetch-category-list))
-              (e/link-to "/registration" "Add Category")]
+              [:a {:onclick (create-categories-js-func) :href "#"} "Add new Category"]]
           [:div
               (f/label :designation "Designation:")
               (f/drop-down :designation-drop (fetch-designation-list))]
